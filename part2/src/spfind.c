2c681f2867e6a5f3549a96621b879fbb3afd6cb6
EmmanuelGacel <123120031+EmmanuelGacel@users.noreply.github.com>
2023-03-27 20:29:12 -0400

    Update spfind.c

--- part2/src/spfind.c
+++ part2/src/spfind.c
@@ -15,7 +15,9 @@
 #include <sys/wait.h>
 #define BUFSIZE 128
 int main(int argc, char** argv){
-    int pipefd[2];
+    int pfind_to_sort[2], sort_to_parent[2];
+    pipe(pfind_to_sort) &&  pipe(sort_to_parent);
+
     if((pipe(pipefd)) == -1){
         fprintf(stderr,"Error: can't create pipe");
         return EXIT_FAILURE;
@@ -26,41 +28,54 @@ int main(int argc, char** argv){
     //printf("Child 2 process %ld .\n",(long)child_2pid);
     if ((child_1pid  || child_2pid ) < 0){
         fprintf(stderr, "Before fork error");
-    }else if(child_1pid == 0){//Inside child 1
-        close (pipefd[0]);
-        dup2(pipefd[1], 1);
-        dup2(pipefd[1], 2);
-        close (pipefd[1]);
+    }else if(child_1pid =fork() == 0){//Inside child 1
+        if(child_1pid < 0){
+            fprintf(stder, "Fork error");
+        }
+        close(pfind_to_sort[0]);
+        dup2(pfind_to_sort[1],STDOUT_FILENO);
+        close(pfind_to_sort[1]);
+        close(sort_to_parent[0]);
+        close(sort_to_parent[1]);
         if (execv("pfind", argv) == -1){//Run pfind with argv as the input
             printf("Error: execlp() failed. %s.\n", argv[2]);
-            fprintf(stderr, "Error: execlp() failed. %s.\n", strerror(errno));
+            //fprintf(stderr, "Error: pfind failed. %s.\n", strerror(errno));
             return EXIT_FAILURE;
         }
-        printf("Child 1 process %ld continued.\n",(long)child_1pid);
-    } else if(child_2pid == 0){//Inside child 2
+    } else if(child_2pid =fork() == 0){//Inside child 2
+
+        if(child_2pid < 0){
+            fprintf(stder, "Fork error");
+        }
         char buf[BUFSIZE];
-        dup2(pipefd[1], 1);
-        dup2(pipefd[1], 2);
-        close (pipefd[1]);
-        read(pipefd[0], buf, sizeof(BUFSIZE));
+        close(pfind_to_sort[1])
+        dup2(pfind_to_sort[0],STDIN_FILENO);
+        close(pfind_to_sort[0]);
+        dup2(sort_to_parent[1],STDOUT_FILENO);
+        close(sort_to_parent[0]);
+        close(sort_to_parent[0]);
+        /*
+        read(stdin, buf, sizeof(BUFSIZE));
         int fd = open("file", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);//
         if (write (fd, buf, sizeof(buf) - 1) != (sizeof(buf) -1)){
             perror("write error");
             return EXIT_FAILURE;
         }
-        if (execlp("sort", "sort", fd, (char *) 0) == -1){
-            printf("Error: execlp() failed. %s.\n", argv[2]);
-            fprintf(stderr, "Error: execlp() failed. %s.\n", strerror(errno));
+        */
+
+        if (execlp("sort", "sort", stdin, (char *) 0) == -1){
+            printf("Error: sort failed. %s.\n", argv[2]);
+            //fprintf(stderr, "Error: execlp() failed. %s.\n", strerror(errno));
             return EXIT_FAILURE;
-        } 
-        close (fd);
-        close (pipefd[0]);
-        printf("Child 2 process %ld continued.\n", (long)child_2pid);
+        }
     }else{//Inside parent
-         close (pipefd[0]);
-        dup2(pipefd[1], 1);
-        dup2(pipefd[1], 2);
-        close (pipefd[1]);
+
+        close(spfind_to_sort[0]);
+        close(pfind_to_sort[1]);
+        dup2(sort_to_parent[0],STDIN_FILENO);
+        close (sort_to_parent[0]);
+        close (sort_to_parent[1]);
+
         printf("Parent process continued.\n");
         /*
         

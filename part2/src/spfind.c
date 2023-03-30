9193232f68c04e512a0fe5af2f9237ddb626c571
EmmanuelGacel <123120031+EmmanuelGacel@users.noreply.github.com>
2023-03-30 09:58:27 -0400

    Update spfind.c

--- part2/src/spfind.c
+++ part2/src/spfind.c
@@ -13,46 +13,38 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#define BUFSIZE 128
+#define BUFSIZE 1024
 int main(int argc, char** argv){
 
-/*	
-    //pipe(pfind_to_sort) &&  pipe(sort_to_parent);
-
-    if((pipe(pipefd)) == -1){
-        fprintf(stderr,"Error: can't create pipe");
-        return EXIT_FAILURE;
-    }
-
-    //(child_1pid = fork()) && (child_2pid = fork()); //Creates two child processes
-    //printf("Child 1 process %ld .\n",(long)child_1pid);
-    //printf("Child 2 process %ld .\n",(long)child_2pid);
-    //if ((child_1pid  || child_2pid ) < 0){
-    //    fprintf(stderr, "Before fork error");
-*/    
-	//int pfind_to_sort[2], sort_to_parent[2];
-	int pfind_to_sort[2] = {0}; // initializes both elements to 0
-	int sort_to_parent[2] = {0}; // initializes both elements to 0
+	int pfind_to_sort[2]; // initializes both elements to 0
+	int sort_to_parent[2]; // initializes both elements to 0
 	pid_t child_1pid, child_2pid;
+    pipe(pfind_to_sort); 
+    pipe(sort_to_parent);
 
-    if((child_1pid =fork()) <= 0){//Inside child 1
+    if((child_1pid = fork()) <= 0){//Inside child 1
         if(child_1pid < 0){
             fprintf(stderr, "Fork error");
+             return EXIT_FAILURE;
         }
+
         close(pfind_to_sort[0]);
         dup2(pfind_to_sort[1],STDOUT_FILENO);
         close(pfind_to_sort[1]);
         close(sort_to_parent[0]);
         close(sort_to_parent[1]);
+
         if (execv("pfind", argv) == -1){//Run pfind with argv as the input
             //printf("Error: execlp() failed. %s.\n", argv[2]);
             fprintf(stderr, "Error: pfind failed. %s.\n", strerror(errno));
             return EXIT_FAILURE;
         }
 	}
+
     if((child_2pid =fork()) <= 0){//Inside child 2
         if(child_2pid < 0){
             fprintf(stderr, "Fork error");
+            return EXIT_FAILURE;
         }
         //char buf[BUFSIZE];
         close(pfind_to_sort[1]);
@@ -61,43 +53,37 @@ int main(int argc, char** argv){
         dup2(sort_to_parent[1],STDOUT_FILENO);
         close(sort_to_parent[0]);
         close(sort_to_parent[1]);
-        /*
-        read(stdin, buf, sizeof(BUFSIZE));
-        int fd = open("file", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);//
-        if (write (fd, buf, sizeof(buf) - 1) != (sizeof(buf) -1)){
-            perror("write error");
-            return EXIT_FAILURE;
-        }
-        */
-
-        if (execlp("sort", "sort", "-u", NULL) == -1){
+       
+        if (execlp("sort", "sort", NULL) == -1){
             printf("Error: sort failed. %s.\n", argv[2]);
             //fprintf(stderr, "Error: execlp() failed. %s.\n", strerror(errno));
             return EXIT_FAILURE;
         }
-    }
-    //Inside parent
-
+    }//Inside of parent
         close(pfind_to_sort[0]);
         close(pfind_to_sort[1]);
         dup2(sort_to_parent[0],STDIN_FILENO);
-        close (sort_to_parent[0]);
-        close (sort_to_parent[1]);
-
-        printf("Parent process continued.\n");
-        /*
+        close(sort_to_parent[0]);
+        close(sort_to_parent[1]);
         
-        if (execvp("sort", argv) == -1){
-            printf("Error: execlp() failed. %s.\n", argv[2]);
-            fprintf(stderr, "Error: execlp() failed. %s.\n", strerror(errno));
-            return EXIT_FAILURE;
+        char buf[BUFSIZE];
+        int file_count = 0;
+        ssize_t count = read(STDIN_FILENO, buf, sizeof(buf) - 1);
+        if (count == -1){
+            perror("read()");
+            exit (EXIT_FAILURE);
         }
-        */
+        buf[count] = '\0';
+        for (int i = 0; buf[i] != '\0'; i++ ){//Counts the number of files found
+            if (buf[i] == '\n') file_count ++;
+        }
+        printf("%s", buf);
+        printf("Total matches: %d\n", file_count);
 
         int status;
         int status1;
         do{//Wait for child 1 to finish
-            pid_t w = waitpid(child_1pid, &status, WUNTRACED  | WCONTINUED);
+            pid_t w = waitpid(child_1pid, &status, 0);//WUNTRACED  | WCONTINUED);
             if (w == -1){//waitpid() failed
                 perror("waitpid()");
                 exit(EXIT_FAILURE);
@@ -116,7 +102,7 @@ int main(int argc, char** argv){
             }
         }while (!WIFEXITED(status) && !WIFSIGNALED(status));
         do{//Wait fo child 2 to finish
-            pid_t w = waitpid(child_2pid, &status1, WUNTRACED| WCONTINUED);
+            pid_t w = waitpid(child_2pid, &status1, 0);//WUNTRACED| WCONTINUED);
             if (w == -1){//waitpid() failed
                 perror("waitpid()");
                 exit(EXIT_FAILURE);
@@ -134,6 +120,6 @@ int main(int argc, char** argv){
                 printf("Child process %ld continued.\n", (long)child_2pid);
             }
         }while (!WIFEXITED(status1) && !WIFSIGNALED(status1));
-	
+
 	return EXIT_SUCCESS;
 }
